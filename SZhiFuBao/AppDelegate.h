f0e043c443c022b506f53ee8d293b14e568da512
Superman <703353105@qq.com>
2018-07-12 16:57:16 +0800

    Initial Commit

---
+++ SZhiFuBao/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SZhiFuBao
+//
+//  Created by Superman on 2018/7/12.
+//  Copyright © 2018年 Superman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
