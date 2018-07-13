6d4ee824e8e5fd8ed58971b23eb3a30d86a9adbc
Superman <703353105@qq.com>
2018-07-13 16:09:42 +0800

    first commit

---
+++ SZhiFuBao/SingleView.h
@@ -0,0 +1,13 @@
+//
+//  SingleView.h
+//  SZhiFuBao
+//
+//  Created by Superman on 2018/7/12.
+//  Copyright © 2018年 Superman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SingleView : UIView
+
+@end
---
+++ SZhiFuBao/SingleView.m
@@ -0,0 +1,21 @@
+//
+//  SingleView.m
+//  SZhiFuBao
+//
+//  Created by Superman on 2018/7/12.
+//  Copyright © 2018年 Superman. All rights reserved.
+//
+
+#import "SingleView.h"
+
+@implementation SingleView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
