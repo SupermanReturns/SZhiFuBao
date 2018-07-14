a976be7980af9cda2e75b851773e5c20537426dc
Superman <703353105@qq.com>
2018-07-14 12:22:36 +0800

    修改代码

--- SZhiFuBao/SingleView.h
+++ SZhiFuBao/SingleView.h
@@ -8,6 +8,21 @@
 
 #import <UIKit/UIKit.h>
 
+@protocol SingleViewDelegate <NSObject>
+
+- (void)clickOneViewReturnTitle:(NSString *)title;
+- (void)beginMoveAction:(NSString *)tag;//移动前
+- (void)moveViewAction:(NSString *)tag gesture:(UILongPressGestureRecognizer *)gesture;//移动中
+- (void)endMoveViewAction:(NSString *)tag;//结束移动
+
+@end
 @interface SingleView : UIView
 
+@property (nonatomic, strong) NSString *title;
+@property (nonatomic, assign) CGPoint viewPoint;
+@property (nonatomic, strong) NSString *tagid;
+
+@property (nonatomic, weak) id<SingleViewDelegate>delegate;
+
+- (instancetype)initWithFrame:(CGRect)frame title:(NSString *)title;
 @end
--- SZhiFuBao/SingleView.m
+++ SZhiFuBao/SingleView.m
@@ -8,7 +8,82 @@
 
 #import "SingleView.h"
 
+@interface SingleView () {
+    UILabel *_label;
+}
+
+@end
+
 @implementation SingleView
+- (instancetype)initWithFrame:(CGRect)frame title:(NSString *)title {
+    if(self=[super initWithFrame:frame]){
+        self.backgroundColor = [UIColor whiteColor];
+        self.title = title;
+        [self initWithLabel];
+    }
+    return self;
+}
+- (void)initWithLabel
+{
+    UILabel *label = [[UILabel alloc] initWithFrame:CGRectMake(0, 50, self.frame.size.width, self.frame.size.height-50)];
+    label.text = _title;
+    label.userInteractionEnabled = YES;
+    label.textAlignment = NSTextAlignmentCenter;
+    label.font = [UIFont systemFontOfSize:15];
+    label.textColor = [UIColor grayColor];
+    [self addSubview:label];
+    _label = label;
+    
+    //长按手势
+    UILongPressGestureRecognizer *longGesture = [[UILongPressGestureRecognizer alloc] initWithTarget:self action:@selector(viewLongPressGesture:)];
+    [self addGestureRecognizer:longGesture];
+    
+    //点击手势
+    UITapGestureRecognizer *tapGesture = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(viewtapGesture:)];
+    [self addGestureRecognizer:tapGesture];
+    
+}
+
+#pragma 长按手势
+- (void)viewLongPressGesture:(UILongPressGestureRecognizer *)gesture
+{
+    switch (gesture.state) {
+            //移动前
+        case UIGestureRecognizerStateBegan:
+            if ([self.delegate respondsToSelector:@selector(endMoveViewAction:)]) {
+                _label.textColor = [UIColor redColor];
+                [self.delegate beginMoveAction:self.tagid];
+            }
+            break;
+            //移动中
+        case UIGestureRecognizerStateChanged:
+            
+            if ([self.delegate respondsToSelector:@selector(moveViewAction:gesture:)]) {
+                
+                [self.delegate moveViewAction:self.tagid gesture:gesture];
+            }
+            break;
+            //移动后
+        case UIGestureRecognizerStateEnded:
+            
+            if ([self.delegate respondsToSelector:@selector(endMoveViewAction:)]) {
+                _label.textColor = [UIColor grayColor];
+                [self.delegate endMoveViewAction:self.tagid];
+            }
+            break;
+            
+        default:
+            break;
+    }
+}
+
+#pragma 轻拍手势
+- (void)viewtapGesture:(UITapGestureRecognizer *)gesture
+{
+    if ([self.delegate respondsToSelector:@selector(clickOneViewReturnTitle:)]) {
+        [self.delegate clickOneViewReturnTitle:_title];
+    }
+}
 
 /*
 // Only override drawRect: if you perform custom drawing.
